6371a82a8aea61368c2a6774534c9db3da056f1f
wu zaiqiang <wuzaiqiang@zju.edu.cn>
2021-12-19 21:34:16 +0800

    add

--- src/include/SeqLoader.h
+++ src/include/SeqLoader.h
@@ -9,23 +9,34 @@ class SeqLoader
 {
 public:
     vector< pcl::PointCloud<pcl::PointXYZ>::Ptr> frames;
+    vector< Eigen::Matrix4f> poses;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr globalPointCloud;
 	SeqLoader(string path)
 	{
         int i = 0;
         while (true)
         {
-            string file_path = path + "/" + "frame-"+makeFixedLength(i,6)+".bin";//frame-000000.bin
+            string frame_path = path + "/" + "frame-"+makeFixedLength(i,6)+".bin";//frame-000000.bin
+            string pose_path = path + "/" + "frame-" + makeFixedLength(i, 6) + ".pose.txt";
             pcl::PointCloud<pcl::PointXYZ>::Ptr target_cloud(new pcl::PointCloud<pcl::PointXYZ>);
             //if (pcl::io::loadPCDFile<pcl::PointXYZ>("../data/full/seq-01/frame-000000.bin", *target_cloud) == -1)
-            if (LoadBinFile(file_path, target_cloud) == -1)
+            if (LoadBinFile(frame_path, target_cloud) == -1)
             {
                 break;
             }
             else
             {
                 frames.push_back(target_cloud);
+                poses.push_back(readMatrix(pose_path.c_str()));
                 i++;
             }
+        }
+        globalPointCloud = pcl::PointCloud<pcl::PointXYZ>::Ptr(new pcl::PointCloud<pcl::PointXYZ>);
+        for (int i = 0; i < frames.size(); i++)
+        {
+            pcl::PointCloud<pcl::PointXYZ> temp;
+            pcl::transformPointCloud(*frames[i], temp, poses[i]);
+            *globalPointCloud += temp;
         }
 	}
 private:
@@ -35,7 +46,7 @@ private:
         std::fstream input(in_file.c_str(), std::ios::in | std::ios::binary);// in_file 为点云bin文件
         if (!input.good())
         {
-            std::cerr << "Could not read file: " << in_file << std::endl;
+            std::cerr << "Finish loading data"<< std::endl;
             return -1;
         }
         input.seekg(0, std::ios::beg);
@@ -64,5 +75,47 @@ private:
         ostr << std::setfill('0') << std::setw(length) << (i < 0 ? -i : i);
 
         return ostr.str();
+    }
+    Eigen::Matrix4f readMatrix(const char filename[])
+    {
+        Eigen::Matrix4f mat;
+        ifstream mmatrix_file(filename);
+        string line;
+        string num;
+        int pos;
+        int row = 0;
+        int col = 0;
+
+        while (mmatrix_file >> line)
+        {
+            for (int p = 0; p < 4; p++)
+            {
+                pos = line.find(",");
+                num = line.substr(0, pos);
+                line.erase(0, pos + 1);
+
+                stringstream x(num);
+                float number = 0;
+                x >> number;
+
+                mat(row,p) = number;
+                ++col;
+            }
+            ++row;
+        }
+        //printMatrix(mat);
+        return mat;
+    }
+    void printMatrix(Eigen::Matrix4f mat)
+    {
+        cout << "mat:" << endl;
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                cout << mat(i, j) << ",";
+            }
+            cout << endl;
+        }
     }
 };
